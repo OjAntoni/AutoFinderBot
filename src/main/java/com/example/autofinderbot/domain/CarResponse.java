719e6d5f71ed597deec3a37a47a97f88e9b7bdda
Anton Ziaziulin <zezyulin90@gmail.com>
2024-12-04 22:30:38 +0200

    Added not based on css classes parsing

--- src/main/java/com/example/autofinderbot/domain/CarResponse.java
+++ src/main/java/com/example/autofinderbot/domain/CarResponse.java
@@ -1,10 +1,27 @@
 package com.example.autofinderbot.domain;
 
-public record CarResponse(
-        String title,
-        String kms,
-        String transmission,
-        String year,
-        String price,
-        String url) {
+import lombok.*;
+import lombok.EqualsAndHashCode.Exclude;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = PRIVATE, makeFinal = true)
+@Getter
+@EqualsAndHashCode
+@ToString
+public class CarResponse {
+    String title;
+    String brand;
+    String fuelType;
+    long mileage;
+    String mileageUnit;
+    double price;
+    String currency;
+    @Exclude
+    @NonFinal
+    @Setter
+    String url;
 }
