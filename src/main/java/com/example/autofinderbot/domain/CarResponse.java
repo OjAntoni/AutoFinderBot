9ce42c83072e36dd3601c090ce173518d2be5ed5
Anton Ziaziulin <zezyulin90@gmail.com>
2024-12-04 10:32:32 +0200

    Changed format in CarResponse

--- src/main/java/com/example/autofinderbot/domain/CarResponse.java
+++ src/main/java/com/example/autofinderbot/domain/CarResponse.java
@@ -1,4 +1,10 @@
 package com.example.autofinderbot.domain;
 
-public record CarResponse(String title, String kms, String transmission, String year, String price, String url) {
+public record CarResponse(
+        String title,
+        String kms,
+        String transmission,
+        String year,
+        String price,
+        String url) {
 }
