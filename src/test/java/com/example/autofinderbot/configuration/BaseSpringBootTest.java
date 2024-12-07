84d6d5b34a04459b9420c716dd3adcb52116a0cd
Anton Ziaziulin <zezyulin90@gmail.com>
2024-12-07 12:38:32 +0200

    Move mocking values to BaseSpringBootTest

--- src/test/java/com/example/autofinderbot/configuration/BaseSpringBootTest.java
+++ src/test/java/com/example/autofinderbot/configuration/BaseSpringBootTest.java
@@ -1,12 +1,29 @@
 package com.example.autofinderbot.configuration;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public abstract class BaseSpringBootTest {
+    @TempDir
+    static Path tempDir;
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) throws IOException {
+        Path tempFilePath = tempDir.resolve("old-test.txt");
+        Files.copy(Paths.get("src/test/resources/old"), tempFilePath);
+        registry.add("telegram.bot.storage.file", tempFilePath::toString);
+    }
 }
--- src/test/java/com/example/autofinderbot/repository/CarFileRepositoryTest.java
+++ src/test/java/com/example/autofinderbot/repository/CarFileRepositoryTest.java
@@ -2,37 +2,21 @@ package com.example.autofinderbot.repository;
 
 import com.example.autofinderbot.configuration.BaseSpringBootTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarFileRepositoryTest extends BaseSpringBootTest {
-    @TempDir
-    static Path tempDir;
 
     @Autowired
     private ConfigurableApplicationContext context;
 
     @Autowired
     CarFileRepository carFileRepository;
-
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) throws IOException {
-        Path tempFilePath = tempDir.resolve("test-file.txt");
-        Files.copy(Paths.get("src/test/resources/old"), tempFilePath);
-        registry.add("telegram.bot.storage.file", tempFilePath::toString);
-    }
 
     @Test
     void loadCarsOnStartUp_PosTC() {
@@ -58,7 +42,7 @@ class CarFileRepositoryTest extends BaseSpringBootTest {
         DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) context.getAutowireCapableBeanFactory();
         registry.destroySingleton("carFileRepository");
 
-        CarFileRepository carFileRepositoryRecreated = new CarFileRepository(/* Pass required dependencies */);
+        CarFileRepository carFileRepositoryRecreated = new CarFileRepository();
         context.getAutowireCapableBeanFactory().autowireBean(carFileRepositoryRecreated);
         context.getAutowireCapableBeanFactory().initializeBean(carFileRepositoryRecreated, "carFileRepository");
         context.getBeanFactory().registerSingleton("carFileRepository", carFileRepositoryRecreated);
