4137283cdacb7820b6f369d45d493030ed57f386
Anton <73666164+OjAntoni@users.noreply.github.com>
2024-12-23 13:27:58 +0100

    GitHub Actions Pipeline
    * Added gitHub actions verification

--- src/test/java/com/example/autofinderbot/configuration/BaseSpringBootTest.java
+++ src/test/java/com/example/autofinderbot/configuration/BaseSpringBootTest.java
@@ -1,18 +1,14 @@
 package com.example.autofinderbot.configuration;
 
-import com.example.autofinderbot.TelegramBot;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -22,10 +18,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(BeanConfig.class)
 @ExtendWith(MockitoExtension.class)
 @Transactional
 public abstract class BaseSpringBootTest {
@@ -55,26 +50,11 @@ public abstract class BaseSpringBootTest {
         Files.copy(Paths.get("src/test/resources/old"), tempFilePath);
 
         registry.add("telegram.bot.storage.file", tempFilePath::toString);
+        registry.add("telegram.bot.token", () -> "token");
 
         registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
         registry.add("spring.liquibase.change-log", () -> "classpath:db/changelog/test/db.changelog-test.yaml");
-    }
-
-    @TestConfiguration
-    static class MockConfig {
-
-        @Bean
-        @Primary
-        public TelegramBot telegramBotMock() {
-            return mock(TelegramBot.class);
-        }
-
-        @Bean
-        @Primary
-        public TelegramClient telegramClientMock() {
-            return mock(TelegramClient.class);
-        }
     }
 }
---
+++ src/test/java/com/example/autofinderbot/configuration/BeanConfig.java
@@ -0,0 +1,23 @@
+package com.example.autofinderbot.configuration;
+
+import com.example.autofinderbot.TelegramBot;
+import com.example.autofinderbot.parser.CarParserService;
+import com.example.autofinderbot.repository.CarFileRepository;
+import com.example.autofinderbot.service.ScheduledExecutor;
+import com.example.autofinderbot.shared.Logger;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class BeanConfig {
+
+    @Bean
+    ScheduledExecutor scheduledExecutor(
+        CarParserService carParserService,
+        CarFileRepository carFileRepository,
+        Logger logger
+    ) {
+        return new ScheduledExecutor(carParserService, carFileRepository, Mockito.mock(TelegramBot.class), logger);
+    }
+}
