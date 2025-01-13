5cd89e23badd64de8ab69f13afe6bdacb8443b24
Anton Ziaziulin <zezyulin90@gmail.com>
2025-01-14 00:13:24 +0200

    deleted unused exceptions

--- src/main/java/com/example/autofinderbot/telegram/exception/TelegramCommandNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.autofinderbot.telegram.exception;
-
-public class TelegramCommandNotFoundException extends TelegramBotException {
-    private static final String ERROR_MESSAGE = "Command %s not found.";
-
-    public TelegramCommandNotFoundException(String command) {
-        super(ERROR_MESSAGE.formatted(command));
-    }
-}
