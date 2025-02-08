ad62ed7f5f87fa1eddf184fa803f082ffa723c54
Anton <73666164+OjAntoni@users.noreply.github.com>
2025-02-08 17:25:55 +0100

    Fixed sql script for user filter (#101)

--- src/main/resources/db/changelog/main/common/021.sql
+++ src/main/resources/db/changelog/main/common/021.sql
@@ -4,7 +4,8 @@
 ALTER TABLE user_filter ADD COLUMN search_url VARCHAR(2048);
 
 UPDATE user_filter SET search_url =
-(SELECT u.search_url FROM users u WHERE u.id = user_filter.user_id) WHERE TRUE;
+(SELECT u.search_url FROM users u WHERE u.id = user_filter.user_id)
+WHERE state = 'NEW';
 
 ALTER TABLE user_filter ALTER COLUMN search_url SET NOT NULL;
 
