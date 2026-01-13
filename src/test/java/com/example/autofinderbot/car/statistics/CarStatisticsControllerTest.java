49292db2f3dbfb1d8199644172050a9954899f49
ojAntoni <zezyulin90@gmail.com>
2026-01-13 13:26:58 +0100

    Split statistics controller tests

--- src/test/java/com/example/autofinderbot/car/statistics/CarStatisticsControllerTest.java
+++ src/test/java/com/example/autofinderbot/car/statistics/CarStatisticsControllerTest.java
@@ -1,17 +1,11 @@
 package com.example.autofinderbot.car.statistics;
 
 import com.example.autofinderbot.configuration.BaseRestApiTest;
-import com.example.autofinderbot.user.UserStatisticsResponse;
 import com.example.autofinderbot.configuration.TestRequestSender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -19,36 +13,14 @@ import java.time.ZoneId;
 import java.util.EnumSet;
 import java.util.List;
 
-class StatisticsControllerTest extends BaseRestApiTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+class CarStatisticsControllerTest extends BaseRestApiTest {
     @Autowired
     private TestRequestSender sender;
-
-    @Test
-    void getUserStatistics_PosTC() {
-        ResponseEntity<UserStatisticsResponse> response = sender.asAdmin("/api/statistics/users/all", GET, null, UserStatisticsResponse.class);
-
-        assertThat(response.getStatusCode())
-            .isEqualTo(OK);
-
-        assertThat(response.getBody())
-            .extracting(
-                UserStatisticsResponse::allUsers,
-                UserStatisticsResponse::hasActiveFilter,
-                UserStatisticsResponse::hasFilter,
-                UserStatisticsResponse::onlineThisWeek,
-                UserStatisticsResponse::onlineToday
-            ).containsExactly(
-                7L, 2L, 3L, 0L, 0L
-            );
-    }
-
-    @Test
-    void unauthorizedGetUserStatistics_NegTC() {
-        ResponseEntity<UserStatisticsResponse> response = sender.unauthorized("/api/statistics/users/all", GET, null, UserStatisticsResponse.class);
-
-        assertThat(response.getStatusCode())
-            .isEqualTo(UNAUTHORIZED);
-    }
 
     @Test
     void getBrandStatistics_PosTC() {
